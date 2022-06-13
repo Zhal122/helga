1d625718fb6203efbefecd7c79433d2c8932a509
Zhal122 <106859781+Zhal122@users.noreply.github.com>
2022-06-13 12:22:47 +0700

    Update anime-loli.js

--- plugins/anime-loli.js
+++ plugins/anime-loli.js
@@ -3,7 +3,7 @@ let api-hyzer = 'https://api.zacros.my.id/randomimg/loli'
     conn.sendButtonImg(m.chat, api-hyzer, 'Nih', wm2, 'NEXT', '.loli', m)
 }
 handler.help = ['loli']
-handler.tags = ['anime']
+handler.tags = ['│⛩️│ANIME']
 handler.command = /^(loli)$/i
 
 module.exports = handler
