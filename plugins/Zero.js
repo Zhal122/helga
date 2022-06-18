ebd0088a3a31d2a01fa9f50f934d7ebf319df389
Zhal122 <106859781+Zhal122@users.noreply.github.com>
2022-06-18 23:46:22 +0700

    Update Zero.js

--- plugins/Zero.js
+++ plugins/Zero.js
@@ -1,5 +1,5 @@
 let handler = async (m, { conn }) => {
-  conn.sendFile(m.chat, 'https://restapi-beta.herokuapp.com/api/zerochan/${command}?apikey=APIKEY, '', '', m)
+  conn.sendFile(m.chat, 'https://restapi-beta.herokuapp.com/api/zerochan/${command}?apikey=Fajar Ihsana, '', '', m)
 }
 handler.help = ['zero']
 handler.tags = ['anime']
